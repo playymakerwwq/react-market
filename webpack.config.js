97afa26da19521fb8e94973f4a500b5e25b300ab
playymakerwwq <arseniygol2017@gmail.com>
2022-06-08 16:32:32 +0300

    something was changed

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
             //     test: /\.json?$/,
             //     exclude: /node_modules/,
             //     loader: 'json-loader'
-            // }
+            // }    
         ]
     },
     resolve: {
